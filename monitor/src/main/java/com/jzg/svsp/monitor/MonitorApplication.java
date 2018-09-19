98c8f5b770a5eeafcec821ec6f851932c40c0150
dikers_admin <dikers_admin@git.cloud.tencent.com>
2018-09-19 10:26:44 +0800

    修改配置文件

--- monitor/src/main/java/com/jzg/svsp/monitor/MonitorApplication.java
+++ monitor/src/main/java/com/jzg/svsp/monitor/MonitorApplication.java
@@ -2,6 +2,7 @@ package com.jzg.svsp.monitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 //import zipkin2.server.internal.EnableZipkinServer;
@@ -11,7 +12,7 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
 @EnableTurbine
 //@EnableZipkinServer
 
-public class MonitorApplication {
+public class MonitorApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonitorApplication.class, args);
