3905adb59883bd49ec2443e7e6dcc55e402f2f1d
dikers_admin <dikers_admin@git.cloud.tencent.com>
2018-09-26 16:32:14 +0800

    修改注册地址

--- gateway/src/main/java/com/jzg/svsp/gateway/filter/TokenFilter.java
+++ gateway/src/main/java/com/jzg/svsp/gateway/filter/TokenFilter.java
@@ -2,14 +2,17 @@ package com.jzg.svsp.gateway.filter;
 
 import com.jzg.svsp.common.enums.HttpStatusEnum;
 import com.jzg.svsp.gateway.config.AuthPropConfig;
+import com.jzg.svsp.gateway.config.RedisClient;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 @Component
 @Slf4j
@@ -18,6 +21,9 @@ public class TokenFilter extends ZuulFilter {
 
     @Autowired
     AuthPropConfig authUrlListConfig;
+
+    @Autowired
+    RedisClient redisClient;
 
     @Override
     public String filterType() {
@@ -36,7 +42,7 @@ public class TokenFilter extends ZuulFilter {
 
     @Override
     public Object run() {
-
+        log.info("authUrlListConfig.getApiUrls()  size  {}  ",  authUrlListConfig.getApiUrls().size());
         if(authUrlListConfig.getApiUrls() == null || authUrlListConfig.getApiUrls().size() ==0 ){
             return null;
         }
@@ -47,12 +53,24 @@ public class TokenFilter extends ZuulFilter {
         HttpServletRequest request = ctx.getRequest();
 //        request.getMethod(),request.getRequestURL().toString()
         log.info("send {} request to {}  ",  request.getServletPath());
-        String accessToken = String.valueOf(request.getParameter("token"));
 
 
+
+        String accessToken =  request.getHeader("token");
+
+        if(StringUtils.isEmpty(accessToken)) {
+            //如果header 中没有取到token  , 从token中尝试取一下。
+            accessToken =  request.getParameter("token");
+        }
+
+        String storeTokenValue = null;
+        if(!StringUtils.isEmpty(accessToken)){
+            storeTokenValue =  redisClient.get(accessToken);
+        }
+        log.info("accessToken ：{}   store value： {}  ",  accessToken , storeTokenValue );
         for(String url : authUrlListConfig.getApiUrls()){
 
-            if(request.getServletPath().startsWith(url)  && (StringUtils.isEmpty(accessToken) || !"111".equals(accessToken))){
+            if(request.getServletPath().startsWith(url)  && (StringUtils.isEmpty(accessToken) || StringUtils.isEmpty(storeTokenValue)) ){
                 log.info("requestPath {} ------------ authUrl {}  ",  request.getServletPath() , url);
                 ctx.setSendZuulResponse(false);
                 ctx.setResponseStatusCode(HttpStatusEnum.UNAUTHORIZED.code());
