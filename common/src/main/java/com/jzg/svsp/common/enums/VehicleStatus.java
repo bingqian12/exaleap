73a29599b1b77e0745927f60faa2a6d5c27dea94
张昌明 <zhangcm@jingzhengu.com>
2018-09-28 15:46:07 +0800

    ''

--- common/src/main/java/com/jzg/svsp/common/enums/VehicleStatus.java
+++ common/src/main/java/com/jzg/svsp/common/enums/VehicleStatus.java
@@ -9,8 +9,8 @@ public enum VehicleStatus {
 
     //车辆状态 0:下架  1:上架  2:已锁定 3:车辆解锁 4:待过户 5:已过户
 
-    UP_SHELF(0,"上架"),
-    DOWN_SHELF(1,"下架"),
+    DOWN_SHELF(0,"下架"),
+    UP_SHELF(1,"上架"),
     LOCK(2,"已锁定"),
     UNLOCK(3,"车辆解锁"),
     WAIT_TRANSFER(4,"待过户"),
