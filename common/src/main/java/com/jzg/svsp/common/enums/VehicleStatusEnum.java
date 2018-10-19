489f8e50f2426905f3af32cbb9f1de0c8860625f
张昌明 <zhangcm@jingzhengu.com>
2018-10-19 20:02:58 +0800

    ''

--- common/src/main/java/com/jzg/svsp/common/enums/VehicleStatusEnum.java
+++ common/src/main/java/com/jzg/svsp/common/enums/VehicleStatusEnum.java
@@ -7,10 +7,11 @@ package com.jzg.svsp.common.enums;
  */
 public enum VehicleStatusEnum {
 
-    //车辆状态  0:未锁定 1:已锁定
+    //车辆状态  0:正常 1:锁定
 
-    UNLOCK(0,"未锁定"),
-    LOCK(1,"已锁定");
+    UNLOCK(0,"正常"),
+    LOCK(1,"锁定"),
+    SOLD(2,"已售");
 
     private int value;
     private String text;
