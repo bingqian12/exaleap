fd7d127d31eb048ef70b7c2a6cfa29daa242d7e8
张昌明 <zhangcm@jingzhengu.com>
2018-10-22 20:52:34 +0800

    'update'

--- common/src/main/java/com/jzg/svsp/common/util/CommonUtil.java
+++ common/src/main/java/com/jzg/svsp/common/util/CommonUtil.java
@@ -4,11 +4,24 @@ import org.apache.commons.lang.StringUtils;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * Created by JZG on 2017/6/12.
  */
 public class CommonUtil {
+
+    /**
+     * 方法二：推荐，速度最快
+     * 判断是否为整数
+     *
+     * @param str 传入的字符串
+     * @return 是整数返回true, 否则返回false
+     */
+    public static boolean isInteger(String str) {
+        Pattern pattern = Pattern.compile("^[-\\+]?[\\d]*$");
+        return pattern.matcher(str).matches();
+    }
 
     /**
      * 转化为不为空的类型String
