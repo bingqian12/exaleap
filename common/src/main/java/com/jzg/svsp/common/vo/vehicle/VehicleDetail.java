86acae5ba63ce768b4f37ed2fed491b84fa22363
张昌明 <zhangcm@jingzhengu.com>
2018-10-26 15:34:06 +0800

    ''

--- common/src/main/java/com/jzg/svsp/common/vo/vehicle/VehicleDetail.java
+++ common/src/main/java/com/jzg/svsp/common/vo/vehicle/VehicleDetail.java
@@ -14,6 +14,8 @@ import java.util.List;
  */
 @Data
 public class VehicleDetail implements BaseVo {
+    private BigDecimal toBPrice;  //直销价
+    private BigDecimal toCPrice;  //分销价
     private Integer checkStatus; //0 初检 1 复检
     private String vehicleBusinessId;  //车辆业务ID
     private String vehicleId;  //车辆ID
